ce4e48236491a64fa743ece54135051448ca985e
franlocus <franciscobuscema@gmail.com>
2023-04-15 23:34:48 -0300

    add CRUD banks with system test

---
+++ app/controllers/banks_controller.rb
@@ -0,0 +1,57 @@
+class BanksController < ApplicationController
+  before_action :set_bank, only: %i[show edit update destroy]
+
+  # GET /banks
+  def index
+    @banks = Bank.all
+  end
+
+  # GET /banks/1
+  def show
+  end
+
+  # GET /banks/new
+  def new
+    @bank = Bank.new
+  end
+
+  # GET /banks/1/edit
+  def edit
+  end
+
+  # POST /banks
+  def create
+    @bank = Bank.new(bank_params)
+
+    if @bank.save
+      redirect_to @bank, notice: t('bank.created')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /banks/1
+  def update
+    if @bank.update(bank_params)
+      redirect_to @bank, notice: t('bank.updated')
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /banks/1
+  def destroy
+    @bank.destroy
+    redirect_to banks_url, notice: t('bank.destroyed')
+  end
+
+  private
+
+  def set_bank
+    @bank = Bank.find(params[:id])
+  end
+
+  def bank_params
+    params.require(:bank).permit(:name)
+  end
+end
---
+++ spec/system/banks_spec.rb
@@ -0,0 +1,98 @@
+require 'rails_helper'
+
+RSpec.describe 'Banks' do
+  let!(:bank) { create(:bank) }
+
+  before do
+    visit banks_path
+  end
+
+  context 'when listing banks' do
+    it 'lists the banks' do
+      expect(page).to have_content(bank.name)
+    end
+  end
+
+  context 'when showing a bank' do
+    it 'shows the bank' do
+      visit bank_path(bank)
+
+      expect(page).to have_content(bank.name)
+    end
+  end
+
+  context 'when creating a bank' do
+    context 'with valid name' do
+      it 'creates a bank' do
+        click_on I18n.t('bank.new')
+        fill_in 'bank_name', with: 'Standard Bank'
+
+        find('input[name="commit"]').click
+        expect(page).to have_content(I18n.t('bank.created'))
+      end
+    end
+
+    context 'with invalid name' do
+      it 'does not create with blank name' do
+        click_on I18n.t('bank.new')
+        fill_in 'bank_name', with: ''
+
+        find('input[name="commit"]').click
+        expect(page).to have_content(I18n.t('errors.messages.blank'))
+      end
+
+      it 'does not create with a long name' do
+        click_on I18n.t('bank.new')
+        fill_in 'bank_name', with: SecureRandom.alphanumeric(60)
+
+        find('input[name="commit"]').click
+        expect(page).to have_content(I18n.t('errors.messages.too_long', count: 50))
+      end
+    end
+  end
+
+  context 'when updating a bank' do
+    context 'with valid name' do
+      it 'updates the bank' do
+        visit edit_bank_path(bank)
+
+        fill_in 'bank_name', with: 'Standard updated Bank'
+
+        find('input[name="commit"]').click
+        expect(page).to have_content(I18n.t('bank.updated'))
+      end
+    end
+
+    context 'with invalid name' do
+      it 'does not update with blank name' do
+        visit edit_bank_path(bank)
+
+        fill_in 'bank_name', with: ''
+
+        find('input[name="commit"]').click
+        expect(page).to have_content(I18n.t('errors.messages.blank'))
+      end
+
+      it 'does not update with a long name' do
+        visit edit_bank_path(bank)
+
+        fill_in 'bank_name', with: SecureRandom.alphanumeric(60)
+
+        find('input[name="commit"]').click
+        expect(page).to have_content(I18n.t('errors.messages.too_long', count: 50))
+      end
+    end
+  end
+
+  context 'when destroying a bank', js: true do
+    it 'destroys the bank' do
+      visit bank_path(bank)
+
+      accept_confirm do
+        click_on I18n.t('bank.destroy')
+      end
+
+      expect(page).to have_content(I18n.t('bank.destroyed'))
+    end
+  end
+end

77b8bc3ca43cf58175178b026c84f391b537dda8
franlocus <franciscobuscema@gmail.com>
2023-04-16 21:51:31 -0300

    refactor flash alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def flash_class(flash_type)
+    case flash_type
+    when 'notice'
+      'bg-green-100 text-green-700'
+    when 'alert'
+      'bg-red-100 text-red-700'
+    else
+      ''
+    end
+  end
 end

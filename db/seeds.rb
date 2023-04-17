cf86b0a3e50912bce23311f398f1ae478dd3687f
franlocus <franciscobuscema@gmail.com>
2023-04-17 13:43:05 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+20.times do |i|
+  Bank.create(name: "Bank N #{i}")
+end
+
+Bank.find_each do |bank|
+  Provider.create(
+    name: "Provider N #{bank.id}",
+    nit: '123456789-1',
+    contact_person_name: 'John Doe',
+    contact_person_number: '123456789',
+    bank_name: bank.name,
+    bank_account_number: '123456789012345'
+  )
+end
